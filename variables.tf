c7194a121344650183c6c5ea8b7a1d8f01b469ad
Ubuntu <jadhavnikhil826@gmail.com>
2026-01-08 16:28:22 +0000

    Updated Region Configs

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "instance_type" {
 variable "regions" {
     description = "List of AWS Regions"
     type        = list(string)
-    default     = [ "us-east-1", "us-east-1", "us-west-2" ]
+    default     = [ "us-east-1", "us-east-2", "us-west-2" ]
 }
 
 # VPC CIDRs
